6c3070a2471651dc3c368a227fba51f4159eece8
SaiDurga-28 <saidurgaanisetti9@gmail.com>
2025-12-07 17:27:48 +0530

    UpdatedAsync&Await

--- Async.js
+++ Async.js
@@ -1,44 +1,60 @@
-// function getUserDetails(id){
-//     return new Promise((resolve, reject) => {
-//         setTimeout(()=>{
-//             resolve({userroll:"678"})
-//         },2000);
-//     })
-// };
-// const myfun=async()=>{
-//     console.log("123");
-//     const result= await getUserDetails("123");
-//     console.log(result);
-//     console.log("456")
-// };
-// myfun();
-function getUserDetails(id) {
-    return new Promise((resolve) => {
-        console.log("getting user details by userID", id); 
-        resolve({ userroll: "678" });
-    });
-}
+// // function getUserDetails(id){
+// //     return new Promise((resolve, reject) => {
+// //         setTimeout(()=>{
+// //             resolve({userroll:"678"})
+// //         },2000);
+// //     })
+// // };
+// // const myfun=async()=>{
+// //     console.log("123");
+// //     const result= await getUserDetails("123");
+// //     console.log(result);
+// //     console.log("456")
+// // };
+// // myfun();
+// function getUserDetails(id) {
+//     return new Promise((resolve) => {
+//         console.log("getting user details by userID", id); 
+//         resolve({ userroll: "678" });
+//     });
+// }
 
-function getUserSubjects(userRoll) {
-    return new Promise((resolve) => {
-        console.log("getting user subjects by roll number", userRoll); 
-        resolve({ subid: "en-1" });
-    });
-}
+// function getUserSubjects(userRoll) {
+//     return new Promise((resolve) => {
+//         console.log("getting user subjects by roll number", userRoll); 
+//         resolve({ subid: "en-1" });
+//     });
+// }
 
-function getUserMarks(userSubId) {
-    return new Promise((resolve) => {
-        console.log("getting user marks with subid", userSubId);
-        resolve("Marks fetched successfully");
-    });
-}
+// function getUserMarks(userSubId) {
+//     return new Promise((resolve) => {
+//         console.log("getting user marks with subid", userSubId);
+//         resolve("Marks fetched successfully");
+//     });
+// }
 
-async function fetchUserData() {
-    const userRoll = await getUserDetails("123");
-    const userSubId = await getUserSubjects(userRoll);
-    const marks = await getUserMarks(userSubId);
+// async function fetchUserData() {
+//     const userRoll = await getUserDetails("123");
+//     const userSubId = await getUserSubjects(userRoll);
+//     const marks = await getUserMarks(userSubId);
 
-    console.log("Final Output:", marks);
-}
+//     console.log("Final Output:", marks);
+// }
 
-fetchUserData();
+// fetchUserData();
+function getUserDetails(id){
+    return new Promise((resolve,reject) => {
+        setTimeout(() => {
+            resolve({userroll: "678"})
+        }, 2000);
+    })
+};
+
+const myfun = async() => {
+    console.log("123")
+    const result = await getUserDetails("123");
+    console.log(result);
+    console.log("456")
+};
+
+myfun();
