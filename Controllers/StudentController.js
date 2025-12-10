399ef6bffde65c4470de2af3b8e4e4c1b8bfc3b2
SaiDurga-28 <saidurgaanisetti9@gmail.com>
2025-12-10 20:34:54 +0530

    MVC_Controller

---
+++ Controllers/StudentController.js
@@ -0,0 +1,19 @@
+const getStudents=(req,res)=>{
+    //Get data from DB(Logic)
+    let stddata={name:"Sai Durga",roll:"23P31A0502"};
+    res.status(200).json({data:stddata});
+};
+const addStudents=(req,res)=>{
+    //Add data
+    res.status(201).json({message:"Data added successfully"});
+}
+const updateStudents=(req,res)=>{
+    //Update data
+    let updatedData={name:"Sai Durga Anisetti",roll:"23P31A0502"};
+    res.status(200).json({data:updatedData});
+}
+const deleteStudents=(req,res)=>{
+    //Delete data
+    res.status(200).json({message:"Data deleted successfully"});
+}
+export {getStudents,addStudents,updateStudents,deleteStudents};
