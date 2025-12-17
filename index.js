29627069adce2f648aeb7149c40892c99b8c1676
SaiDurga-28 <saidurgaanisetti9@gmail.com>
2025-12-17 20:38:31 +0530

    API Calling using Axios and Middleware Cors

--- index.js
+++ index.js
@@ -30,29 +30,33 @@
 // });
 //Middleware
 import express from 'express';
+import cors from 'cors';
+import router from './Routes/StudentRouter.js';
 const app = express();
+app.use(cors());//Middleware
 app.use(express.json());//Middleware
+app.use('/',router);
 app.get('/get-users', (req, res) => {
     res.send("Api success");
 })
-app.put('/edit-users', (req, res) => {
-    let mydata=req.body;
-    console.log(mydata);//undefined if not using middleware
-    //logic to add data into your database
-    res.send("data edited");
-})
-app.post('/add-users', (req, res) => {
-    let data=req.body;
-    console.log(data);//undefined if not using middleware
-    //logic to add data into your database
-    res.send("data added");
-})
-app.delete('/delete-users',(req,res)=>{
-    let data=req.body;
-    console.log(data);//undefined if not using middleware
-    //logic to add data into your database
-    res.send("data deleted");
-})
+// app.put('/edit-users', (req, res) => {
+//     let mydata=req.body;
+//     console.log(mydata);//undefined if not using middleware
+//     //logic to add data into your database
+//     res.send("data edited");
+// })
+// app.post('/add-users', (req, res) => {
+//     let data=req.body;
+//     console.log(data);//undefined if not using middleware
+//     //logic to add data into your database
+//     res.send("data added");
+// })
+// app.delete('/delete-users',(req,res)=>{
+//     let data=req.body;
+//     console.log(data);//undefined if not using middleware
+//     //logic to add data into your database
+//     res.send("data deleted");
+// })
 app.listen(7007, () => {
     console.log(`server running at port ${7007}`);
 });
